f9e86199ebe5d10873876be5172f1db3698129fc
timothy <williamstim146@gmail.com>
2023-03-16 13:08:07 -0500

    Create expensesrepository extending jparepository interfacing with expensesmodel. Create post routing to /api/expenses/add which also validates incoming jwt to be able to add expenses to the appropriate user.

--- src/main/java/com/example/budgetappbackend/controller/ExpenseListController.java
+++ src/main/java/com/example/budgetappbackend/controller/ExpenseListController.java
@@ -1,5 +1,6 @@
 package com.example.budgetappbackend.controller;
 
+import com.example.budgetappbackend.repository.ExpensesRepository;
 import com.example.budgetappbackend.shared.KeyProperties;
 import com.google.gson.Gson;
 import io.jsonwebtoken.Jws;
@@ -16,15 +17,31 @@ public class ExpenseListController {
     // will need to create the expense list database model and repository interface
     // will need to create that variable and then load in the value of the variable with the constructor
     // until then, just check to make sure that the jwt is sending with requests
-    Gson gson = new Gson();
+    private final ExpensesRepository expensesRepository;
 
-    public ExpenseListController() {
+    private static final Gson gson = new Gson();
+
+    public ExpenseListController(ExpensesRepository expensesRepository) {
+        this.expensesRepository = expensesRepository;
     }
 
     @CrossOrigin
     @GetMapping
     public ResponseEntity listOfExpenses(@RequestHeader("Authorization") String authorization) {
         // now I just need to authenticate the jwt that was sent as authorization and return the expense list with the given sorting parameters that I will add later
+        try {
+            Jws jws = Jwts.parserBuilder().setSigningKey(KeyProperties.getPublicKey()).build().parseClaimsJws(authorization);
+            Object body = jws.getBody();
+            System.out.println("Valid jwt");
+            return ResponseEntity.ok(gson.toJson(authorization));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("");
+        }
+    }
+
+    @CrossOrigin
+    @PostMapping("/add")
+    public ResponseEntity addExpense(@RequestHeader("Authorization") String authorization) {
         try {
             Jws jws = Jwts.parserBuilder().setSigningKey(KeyProperties.getPublicKey()).build().parseClaimsJws(authorization);
             Object body = jws.getBody();
--- src/main/java/com/example/budgetappbackend/repository/ExpensesRepository.java
+++ src/main/java/com/example/budgetappbackend/repository/ExpensesRepository.java
@@ -1,4 +1,10 @@
 package com.example.budgetappbackend.repository;
 
-public interface ExpensesRepository {
+import com.example.budgetappbackend.model.Expenses;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExpensesRepository extends JpaRepository<Expenses, Long> {
+    List<Expenses> findByUser_id(Long user_id);
 }
