3fb5a28bf3cddcf5fb13ed7754da978e48dd23fb
timothy <williamstim146@gmail.com>
2023-03-30 15:23:50 -0500

    Add MyErrorController controller that maps to "/error" and overwrites the base whitelabel error handling by sending index.html again but to display the Page Not Found page within Angular accessed by the router.

--- src/main/java/com/example/budgetappbackend/controller/MyErrorController.java
+++ src/main/java/com/example/budgetappbackend/controller/MyErrorController.java
@@ -1,15 +1,16 @@
 package com.example.budgetappbackend.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
-public class AppController {
+public class MyErrorController implements ErrorController {
 
-    @RequestMapping(value = { "", "/", "/addexpense", "/expenselist" })
-    public String homePage(Map<String, Object> model) {
+    @RequestMapping(value = "/error")
+    public String handleError(Map<String, Object> model) {
         return "index.html";
     }
 
