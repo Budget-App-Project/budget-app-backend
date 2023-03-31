7a4093e9da409c15a1e483277827a66050811aec
timothy <williamstim146@gmail.com>
2023-03-31 18:53:13 -0500

    Add necessary additions and changes in order for the files to be bundled as a war file and run on a tomcat server.

--- src/main/java/com/example/budgetappbackend/BudgetAppBackendApplication.java
+++ src/main/java/com/example/budgetappbackend/BudgetAppBackendApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BudgetAppBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BudgetAppBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetAppBackendApplication.class, args);
+    }
 
 }
